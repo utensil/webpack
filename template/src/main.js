d1af9cecfe620d2219d36ec5a8c859614e594cf0
utensil <utensilcandel@gmail.com>
2016-03-05 23:47:56 +0800

    Add Jade, Sass, vue-resource, vue-router, vuex and vueboot

--- template/src/main.js
+++ template/src/main.js
@@ -1,5 +1,13 @@
 import Vue from 'vue'
 import App from './App'
+import VueResource from 'vue-resource'
+import Vuex from 'vuex'
+import VueRouter from 'vue-router'
+// import { modal } from 'vueboot'
+
+Vue.use(VueResource)
+Vue.use(Vuex)
+Vue.use(VueRouter)
 
 /* eslint-disable no-new */
 new Vue({
